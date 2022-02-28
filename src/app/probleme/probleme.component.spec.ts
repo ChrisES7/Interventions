d594e856130dfdf5281e88be676dea3c17b09957
ChristopherE7 <kikoskaf@gmail.com>
2022-02-28 13:41:04 -0500

    Test6

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -27,7 +27,7 @@ describe('ProblemeComponent', () => {
 
   it('Champ Prenom doit comporter au moins 3 caracteres',() => {
     let zone = component.problemeForm.controls['Prenom']
-    zone.setValue(' '.repeat(10))
+    zone.setValue('  a'.repeat(1));
     expect(zone.valid).toBeTruthy();
   });
 });
